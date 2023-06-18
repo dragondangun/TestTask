59ad015612c22401789d9deb01ad87c99eea33f6
Danil Shkurat <dan.sura@mail.ru>
2023-06-18 12:16:18 +0300

    Get area of circle

--- Test/GetArea.cs
+++ Test/GetArea.cs
@@ -1,7 +1,15 @@
 ﻿namespace Test {
     public static class GetArea {
         public static double OfCircle(IRadiusProvider circle) {
-            throw new NotImplementedException();
+            if(circle is null)
+                throw new ArgumentNullException("Circle must be not null");
+
+            double radius = circle.GetRadius();
+            if(radius < 0) {
+                throw new ArgumentException("Radius must be greater or equal to 0");
+            }
+
+            return radius*radius*Math.PI;
         }
 
         public static double OfTriangle(ITriangleSidesProvider sides) {
