e94ce4d7b8632772514c7a4b7879c4c693ecfbad
Danil Shkurat <dan.sura@mail.ru>
2023-06-18 11:06:13 +0300

    Add radius provider interface

---
+++ Test/IRadiusProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+    public interface IRadiusProvider {
+        public double GetRadius();
+    }
+}
