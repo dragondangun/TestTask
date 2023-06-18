5e4ad6d7c4499bdaa12a089f0e3b446b025afe18
Danil Shkurat <dan.sura@mail.ru>
2023-06-18 12:24:54 +0300

    Rename ITriangleSidesProvides to ISidesProvider

---
+++ Test/GetPerimeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+    public static class GetPerimeter {
+        public static double GetPerimeterOf(IEnumerable<double> sides) {
+            throw new NotImplementedException();
+        }
+    }
+}
