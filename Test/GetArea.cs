e9535b09b8dfa5e0c8c881bc7f87cd09694df6fc
Danil Shkurat <dan.sura@mail.ru>
2023-06-18 11:48:26 +0300

    Add function w/o realisation

---
+++ Test/GetArea.cs
@@ -0,0 +1,11 @@
+﻿namespace Test {
+    public static class GetArea {
+        public static double OfCircle(IRadiusProvider circle) {
+            throw new NotImplementedException();
+        }
+
+        public static double OfTriangle(ITriangleSidesProvider sides) {
+            throw new NotImplementedException();
+        }
+    }
+}
