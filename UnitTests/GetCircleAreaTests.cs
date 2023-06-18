ee3082e23d9e60ae215eb857f40518bf0c6ddbdc
Danil Shkurat <dan.sura@mail.ru>
2023-06-18 12:01:11 +0300

    Tests for get area of circle

---
+++ UnitTests/GetCircleAreaTests.cs
@@ -0,0 +1,82 @@
+using Test;
+
+namespace TestsTest {
+    [TestClass]
+    public class GetCircleAreaTests {
+        [TestMethod]
+        public void GetCircleArea_9() {
+            // Arrange
+            Circle circle = new(9);
+            double expected = 81 * Math.PI;
+            double error = 0.000001;
+            // Act
+            double given = GetArea.OfCircle(circle);
+            // Assert
+            bool result = Math.Abs(expected - given) < error;
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetCircleArea_4d5() {
+            // Arrange
+            Circle circle = new(4.5);
+            double expected = 20.25 * Math.PI;
+            double error = 0.000001;
+            // Act
+            double given = GetArea.OfCircle(circle);
+            // Assert
+            bool result = Math.Abs(expected - given) < error;
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetCircleArea_0() {
+            // Arrange
+            Circle circle = new(9);
+            double expected = 0;
+            double error = 0.000001;
+            // Act
+            double given = GetArea.OfCircle(circle);
+            // Assert
+            bool result = Math.Abs(expected - given) < error;
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetCircleArea_n3() {
+            // Arrange
+            Circle circle = new(9);
+            // Act
+            double given = GetArea.OfCircle(circle);
+            // Assert
+            Assert.ThrowsException<ArgumentException>(() => GetArea.OfCircle(circle));
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetCircleArea_null() {
+            // Arrange
+            Circle circle = null;
+            // Act
+            // Assert
+
+            Assert.ThrowsException<NullReferenceException>(() => GetArea.OfCircle(circle));
+        }
+    }
+
+    class Circle : IRadiusProvider {
+        double radius;
+        double Radius {
+            get => radius;
+            set => radius = value;
+        }
+        
+        public Circle(double r) {
+            radius = r;
+        }
+
+        public double GetRadius() => Radius;
+    }
+}
