08e2a2304b3766b1f205388dc263927e293a4467
Danil Shkurat <dan.sura@mail.ru>
2023-06-19 09:37:50 +0300

    Final version / delete comment and add alias

--- SQL test/SqlTest.sql
+++ SQL test/SqlTest.sql
@@ -1,9 +1,6 @@
--- Мне кажется, что я здесь где-то ошибся, хоть и понимаю джойны и связь многое ко многим,
--- но всё равно не уверен. 
-
-SELECT productName, categoryName FROM Products
+SELECT productName, categoryName FROM products AS p
     LEFT JOIN 
-    (SELECT productId, categoryId FROM Products AS p
-        LEFT JOIN ProductsCategories AS pc ON p.productId = pc.productId) AS r
+    (SELECT p.productId, categoryId FROM products AS p
+        LEFT JOIN productsCategories AS pc ON p.productId = pc.productId) AS r
         ON r.productId=p.productId
         LEFT JOIN Categories AS c ON r.categoryId=c.categoryId 
