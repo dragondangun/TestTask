b4e4a4998cdf525280880657c6c78f6cea4058bb
dragondangun <34654578+dragondangun@users.noreply.github.com>
2023-06-21 09:27:43 +0300

    Delete SqlTest.sql

--- SQL test/SqlTest.sql
+++
@@ -1,6 +0,0 @@
-SELECT productName, categoryName FROM products AS p
-    LEFT JOIN 
-    (SELECT p.productId, categoryId FROM products AS p
-        LEFT JOIN productsCategories AS pc ON p.productId = pc.productId) AS r
-        ON r.productId=p.productId
-        LEFT JOIN Categories AS c ON r.categoryId=c.categoryId 
